5824d94210f71cadfb08d2fe3e0c190e26eb532d
Endlessdaydream <endlessdaydream@qq.com>
2023-08-17 23:54:14 +0800

    rename

---
+++ LearnOpenGL/learn_gl/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Endless_CG_Projects/LearnOpenGL/learn_gl/draw_polygon/draw_triangle/main.cpp" "CMakeFiles/main.dir/draw_polygon/draw_triangle/main.cpp.obj" "msvc" "CMakeFiles/main.dir/draw_polygon/draw_triangle/main.cpp.obj.d"
+  "E:/Endless_CG_Projects/LearnOpenGL/learn_gl/template/depends/glad/glad.cpp" "CMakeFiles/main.dir/template/depends/glad/glad.cpp.obj" "msvc" "CMakeFiles/main.dir/template/depends/glad/glad.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LearnOpenGL/learn_gl/cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/draw_polygon/draw_triangle/main.cpp.obj"
+  "CMakeFiles/main.dir/draw_polygon/draw_triangle/main.cpp.obj.d"
+  "CMakeFiles/main.dir/template/depends/glad/glad.cpp.obj"
+  "CMakeFiles/main.dir/template/depends/glad/glad.cpp.obj.d"
+  "main.exe"
+  "main.exe.manifest"
+  "main.lib"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
